96d9bc19befe04a13a7e13391e3833f7cebbc6d0
Nils <its.just.nils@gmail.com>
2018-03-04 22:02:01 +0100

    added a game tile

---
+++ Assets/Game/Scripts/Tiles/Game/GameTile.cs
@@ -0,0 +1,10 @@
+namespace CCore.Senary.Tiles
+{
+    public class GameTile : Tile
+    {
+        public GameTile(int x, int y, TileType tileType, TileState tileState)
+            : base(x, y, tileType, tileState)
+        {
+        }
+    }
+}
