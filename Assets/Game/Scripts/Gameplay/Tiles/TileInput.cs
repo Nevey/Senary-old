8ab3250eae1f5c358118f8f63aab376fce6dd740
Nils <its.just.nils@gmail.com>
2018-03-11 11:38:32 +0100

    added support for tapping on tiles

---
+++ Assets/Game/Scripts/Gameplay/Tiles/TileInput.cs
@@ -0,0 +1,47 @@
+using System;
+using CCore.Senary.Input;
+using UnityEngine;
+
+namespace CCore.Senary.Gameplay.Tiles
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(TileData))]
+    public class TileInput : MonoBehaviour
+    {
+        private new Collider collider;
+
+        private TileData tileData;
+
+        private void Awake()
+        {
+            collider = GetComponent<Collider>();
+
+            tileData = GetComponent<TileData>();
+        }
+
+        private void OnEnable()
+        {
+            PlayerInput.Instance.TapEvent += OnTap;
+        }
+
+        private void OnDisable()
+        {
+            PlayerInput.Instance.TapEvent -= OnTap;
+        }
+
+        private void OnTap(Vector2 position)
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(position);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider == collider)
+                {
+                    Log("Tapped on tile {0}", tileData.Tile.Name);
+                }
+            }
+        }
+    }
+}
--- Assets/Game/Scripts/Input/PlayerInput.cs
+++ Assets/Game/Scripts/Input/PlayerInput.cs
@@ -1,18 +1,44 @@
 using System;
 using CCore.Input;
+using UnityEngine;
 
 namespace CCore.Senary.Input
 {
     public class PlayerInput : MonoBehaviourSingleton<PlayerInput>
     {
+        public event Action<Vector2> TapEvent;
+
         private void Awake()
         {
             MouseInput.Instance.InputDownEvent += OnMouseDown;
+
+            TouchInput.Instance.InputEvent += OnTouch;
         }
 
         private void OnMouseDown(object sender, MouseInputArgs e)
         {
-            // throw new NotImplementedException();
+            if (e.keyCode == KeyCode.Mouse0)
+            {
+                Tap(e.position);
+            }
+        }
+
+        private void OnTouch(object sender, TouchInputArgs e)
+        {
+            if (e.touchPhase == TouchPhase.Began)
+            {
+                Tap(e.position);
+            }
+        }
+
+        private void Tap(Vector2 position)
+        {
+            if (TapEvent != null)
+            {
+                Log("Tapping screen at position {0}", position);
+                
+                TapEvent(position);
+            }
         }
     }
 }
