6f2b2510a19e08707ed63016ea83a783d05f2bce
Nils <its.just.nils@gmail.com>
2018-03-24 10:33:24 +0100

    tiny revamp on battle view ui

---
+++ Assets/Game/Scripts/UI/BattleResultViews/CenterBattleResultView.cs
@@ -0,0 +1,12 @@
+using CCore.UI;
+
+namespace CCore.Senary.UI
+{
+    public class CenterBattleResultView : UIView
+    {
+        protected override void Setup()
+        {
+            
+        }
+    }
+}
